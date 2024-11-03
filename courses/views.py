e5fd501155fc46a2cbffe4b652567abb1de19254
Robins Side <robinssi@student.21-school.ru>
2024-11-03 14:08:00 +0500

    All Ready

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,5 @@
 # Create your views here.
 
-from django.db.models import Q
 from django.http import FileResponse, HttpResponse
 from django.shortcuts import get_object_or_404, render
 
@@ -9,37 +8,35 @@ from courses.models import Courses, SciencesUni, LessonsUni
 
 
 def index(request):
+    search_query = request.GET.get('page-header-search-input', '').strip()  # Qidiruv uchun input qiymati
     selected_science = request.GET.get('science')
     selected_course = request.GET.get('course')
-    search_query = request.GET.get('page-header-search-input', '').strip()  # Get search input
-    all_lessons_school = LessonsSchool.objects.all()
-    all_lessons_uni = LessonsUni.objects.all()
 
-    # Filter lessons based on search query, science, or course selection
+    # Default - barcha darslar
+    lessons_school = LessonsSchool.objects.all()
+    lessons_uni = LessonsUni.objects.all()
+
+    # Qidiruv bo‘yicha filtr
     if search_query:
-        lessons_school = all_lessons_school.filter(Q(lessonName__icontains=search_query))
-        lessons_uni = all_lessons_uni.filter(Q(name__icontains=search_query))
-    else:
-        lessons_school = all_lessons_school
-        lessons_uni = all_lessons_uni
+        lessons_school = lessons_school.filter(lessonName__icontains=search_query)
+        lessons_uni = lessons_uni.filter(name__icontains=search_query)
 
-    # Handle filtering based on selected science
-    if selected_science:
-        lessons_school = LessonsSchool.objects.filter(science__scienceName=selected_science)
-
-    # Handle filtering based on selected course
-    if selected_course:
-        lessons_uni = LessonsUni.objects.filter(science__name=selected_course)
+    # Fan yoki kurs asosida filtr
+    elif selected_science:
+        lessons_school = lessons_school.filter(science__scienceName=selected_science)
+        lessons_uni = lessons_uni.none()  # School uchun filtrlanganda uni darslari bo'sh
+    elif selected_course:
+        lessons_uni = lessons_uni.filter(science__name=selected_course)
+        lessons_school = lessons_school.none()  # Uni uchun filtrlanganda school darslari bo'sh
 
     context = {'title': "Javlon Domla Zo'r", 'classesSchool': Classes.objects.all(),
-               'coursesSchool': Courses.objects.all(), 'SciencesSchoolwithNamesClasses': list(
+        'coursesSchool': Courses.objects.all(), 'SciencesSchoolwithNamesClasses': list(
             zip([str(name.classes) for name in SciencesSchool.objects.all()],
                 [science.scienceName for science in SciencesSchool.objects.all()])),
-               'SciencesUniwithNamesCourses': list(zip([str(name.course) for name in SciencesUni.objects.all()],
-                                                       [science.name for science in SciencesUni.objects.all()])),
-               'Lessonschool': lessons_school, 'Lessonsuni': lessons_uni, 'selected_science': selected_science,
-               'selected_course': selected_course, 'search_query': search_query, }
-
+        'SciencesUniwithNamesCourses': list(zip([str(name.course) for name in SciencesUni.objects.all()],
+            [science.name for science in SciencesUni.objects.all()])), 'Lessonschool': lessons_school,
+        'Lessonsuni': lessons_uni, 'selected_science': selected_science, 'selected_course': selected_course,
+        'search_query': search_query, }
     return render(request, 'index.html', context)
 
 
